7a22d373dcacd93e0be8ce30afe18f1bf455d201
Rain Kütt <rain.kutt@khk.ee>
2018-09-28 12:42:10 +0300

    seosed railsis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_074120) do
+ActiveRecord::Schema.define(version: 2018_09_28_091714) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_074120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "featured", default: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
